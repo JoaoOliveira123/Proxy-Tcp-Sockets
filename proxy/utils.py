83a4622c88cb82c3e358076fbed165a58fa9c560
Joao Oliveira <jmfqoliveira3@gmail.com>
2022-03-17 16:15:00 -0300

    I finished the hexdumper

--- proxy/utils.py
+++ proxy/utils.py
@@ -15,8 +15,18 @@ class ResponseDataCopier(IResponseDataHandler):
 
 #Organized is why it 
 class BytesToHexOrganizedDumper(IOrganizedDumper):
-    def dump(self, src: bytes, length: int):
-        future_result = []
-        digits = 2
-        for i in range(0, len(src), length):
-            future_result.append(self.hexdump_bytes(src, digits))
+    digits = 2
+
+    def __init__(self):
+        self.__result = []
+
+    def dump(self, src: bytes, line_length: int=16):
+        for i in self.hexdump(src, line_length):
+            self.__result.append(i)
+    
+    def hexdump(self, src: bytes, line_length: int=16):
+        for i in range(0, len(src), line_length):
+            s = src[i:i+line_length]
+            hexa = b' '.join([b'%0*X' % (self.digits, x) for x in s])
+            text = b''.join(x if 0x20 <= ord(x) < 0x7f else b'.' for x in s)
+            yield b''
